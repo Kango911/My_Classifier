0d46b035b79ad3ef7e38b3fb95a15e33de3fd53f
Kango911 <lipenkov.a61@gmail.com>
2024-05-20 00:53:51 +0500

    add bot fun

--- script.py
+++ script.py
@@ -1,25 +1,24 @@
-import sys
 import numpy as np
-import pickle
 import re
 from Stemmer import Stemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score
+import telebot
+
+from core import BOT_TOKEN
 
 
-# очистка текста с помощью regexp приведение слов в инфинитив и нижний регистр, замена цифр
+# Очистка текста с помощью regexp, приведение слов в инфинитив и нижний регистр, замена цифр
 
 def text_cleaner(text):
-    text = text.lower()  # приведение в lowercase
+    text = text.lower()  # Приведение в lowercase
     stemmer = Stemmer('russian')
     text = ' '.join(stemmer.stemWords(text.split()))
-    text = re.sub(r'\b\d+\b', ' digit ', text)  # замена цифр
+    text = re.sub(r'\b\d+\b', ' digit ', text)  # Замена цифр
     return text
 
-
-# загрузка данных из файла model.txt
+# Загрузка данных из файла model.txt
 
 def load_data():
     data = {'text': [], 'tag': []}
@@ -30,7 +29,6 @@ def load_data():
                 data['text'] += [row[0]]
                 data['tag'] += [row[1]]
     return data
-
 
 # Обучение
 
@@ -48,28 +46,28 @@ def train_test_split(data, validation_split=0.1):
         'test': {'x': X[-nb_validation_samples:], 'y': Y[-nb_validation_samples:]}
     }
 
+data = load_data()
+D = train_test_split(data)
 
-# - - - -
+text_clf = Pipeline([
+    ('tfidf', TfidfVectorizer()),
+    ('clf', SGDClassifier(loss='hinge')),
+])
 
-def openai():
-    data = load_data()
-    D = train_test_split(data)
-    text_clf = Pipeline([
-        ('tfidf', TfidfVectorizer()),
-        ('clf', SGDClassifier(loss='hinge')),
-    ])
-    text_clf.fit(D['train']['x'], D['train']['y'])
-    predicted = text_clf.predict(D['train']['x'])
+text_clf.fit(D['train']['x'], D['train']['y'])
 
-    # Начало тестирования программы
+bot = telebot.TeleBot(BOT_TOKEN)
 
-    z = input("Введите вопрос без знака вопроса на конце: ")
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+ bot.reply_to(message, "Hi! I'm K9 classifier bot.")
+
+@bot.message_handler(func=lambda message: True)
+def reply_to_message(message):
+    z = message.text
     zz = []
     zz.append(z)
     predicted = text_clf.predict(zz)
-    print(predicted[0])
+    bot.reply_to(message, predicted[0])
 
-
-# - - - -
-if __name__ == '__main__':
-    sys.exit(openai())
+bot.polling()
